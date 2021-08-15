c35616e748b069553f7aa271712e69f57e52a0fc
Sameer-Ehsanmund <sameer.baigs321@gmail.com>
2021-08-15 11:01:11 -0700

    Environment & Camera Stability

---
+++ Mobile Car Racing Game/Assets/Scripts/cameraStability.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraStability : MonoBehaviour
+{
+
+    public GameObject Car;
+
+    public float CarX;
+    public float CarY;
+    public float CarZ;
+
+    void Update()
+    {
+
+        CarX = Car.transform.eulerAngles.x;
+        CarY = Car.transform.eulerAngles.y;
+        CarZ = Car.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(CarX - CarX, CarY, CarZ - CarZ);
+    }
+}
