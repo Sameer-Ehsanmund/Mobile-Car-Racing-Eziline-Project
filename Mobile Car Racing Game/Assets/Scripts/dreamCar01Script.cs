4ae069c994e32ad34b84fb0d7a631b61777aeebc
Sameer-Ehsanmund <sameer.baigs321@gmail.com>
2021-08-17 01:10:20 -0700

    Enemy Car AI

---
+++ Mobile Car Racing Game/Assets/Scripts/dreamCar01Script.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dreamCar01Script : MonoBehaviour
+{
+    public GameObject theMarker;
+
+    public GameObject[] markers;
+
+    //public GameObject mark1;
+    //public GameObject mark2;
+    //public GameObject mark3;
+
+    public int markTracker;
+
+    void Update()
+    {
+
+        foreach (GameObject marker in markers)
+        {
+
+                //if (markTracker == i)
+                //{
+
+                    theMarker.transform.position = marker.transform.position;
+                //}
+                
+            
+
+            
+            //Debug.Log("okok");
+        }
+
+        //if (markTracker == 0)
+        //{
+
+        //    theMarker.transform.position = mark1.transform.position;
+        //}
+
+        //if (markTracker == 1)
+        //{
+
+        //    theMarker.transform.position = mark2.transform.position;
+        //}
+
+        //if (markTracker == 2)
+        //{
+
+        //    theMarker.transform.position = mark3.transform.position;
+        //}
+
+        //if (markTracker == 3)
+        //{
+
+        //    theMarker.transform.position = mark04.transform.position;
+        //}
+
+        //if (markTracker == 4)
+        //{
+
+        //    theMarker.transform.position = mark05.transform.position;
+        //}
+
+        //if (markTracker == 5)
+        //{
+
+        //    theMarker.transform.position = mark06.transform.position;
+        //}
+    }
+
+    IEnumerator OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.gameObject.tag == "Dreamcar01")
+        {
+
+            this.GetComponent<BoxCollider>().enabled = false;
+
+            markTracker += 1;
+
+            if (markTracker == 3)
+            {
+
+                markTracker = 0;
+            }
+
+            yield return new WaitForSeconds(1);
+            this.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
