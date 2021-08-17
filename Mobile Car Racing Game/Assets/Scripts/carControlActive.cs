2a27dd925c7cf42d684947b7a35655cc7fb1d889
Sameer-Ehsanmund <sameer.baigs321@gmail.com>
2021-08-16 21:59:09 -0700

    count down completed

---
+++ Mobile Car Racing Game/Assets/Scripts/carControlActive.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class carControlActive : MonoBehaviour
+{
+    public GameObject carControl;
+    //public GameObject Dreamcar01;
+
+    void Start()
+    {
+
+        carControl.GetComponent<CarController>().enabled = true;
+        carControl.GetComponent<CarUserControl>().enabled = true;
+        //Dreamcar01.GetComponent("CarAIControl").enabled = true;
+    }
+}
---
+++ Mobile Car Racing Game/Assets/Scripts/countDownScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class countDownScript : MonoBehaviour
+{
+    public GameObject countDown;
+
+    public AudioSource getReady;
+    public AudioSource goAudio;
+
+    public GameObject lapTimer;
+    public GameObject carControls;
+
+    //public AudioSource levelMusic;
+
+    void Start()
+    {
+
+        StartCoroutine(countStart());
+    }
+
+    IEnumerator countStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+        countDown.GetComponent<TextMeshProUGUI>().text = "3";
+        getReady.Play();
+        countDown.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+        countDown.SetActive(false);
+        countDown.GetComponent<TextMeshProUGUI>().text = "2";
+        getReady.Play();
+        countDown.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+        countDown.SetActive(false);
+        countDown.GetComponent<TextMeshProUGUI>().text = "1";
+        getReady.Play();
+        countDown.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+        countDown.SetActive(false);
+        goAudio.Play();
+        //levelMusic.Play();
+
+        lapTimer.SetActive(true);
+        carControls.SetActive(true);
+    }
+}
