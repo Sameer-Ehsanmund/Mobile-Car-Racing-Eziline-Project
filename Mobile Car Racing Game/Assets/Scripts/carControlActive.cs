2cbcde7132f445798088b4f6880e1107711a0608
Sameer-Ehsanmund <sameer.baigs321@gmail.com>
2021-08-17 22:46:04 -0700

    android implementation started

--- Mobile Car Racing Game/Assets/Scripts/carControlActive.cs
+++ Mobile Car Racing Game/Assets/Scripts/carControlActive.cs
@@ -6,13 +6,13 @@ using UnityStandardAssets.Vehicles.Car;
 public class carControlActive : MonoBehaviour
 {
     public GameObject carControl;
-    //public GameObject Dreamcar01;
+    public GameObject Dreamcar01;
 
     void Start()
     {
 
         carControl.GetComponent<CarController>().enabled = true;
         carControl.GetComponent<CarUserControl>().enabled = true;
-        //Dreamcar01.GetComponent("CarAIControl").enabled = true;
+        Dreamcar01.GetComponent<CarAIControl>().enabled = true;
     }
 }
