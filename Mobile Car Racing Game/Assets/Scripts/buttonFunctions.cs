88a843100f45f976ddaeef0632a3b9ca516f48f5
Sameer-Ehsanmund <sameer.baigs321@gmail.com>
2021-08-20 04:27:43 -0700

    UI and scene management

---
+++ Mobile Car Racing Game/Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+
+    public void startGame()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+    public void selectTrack()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void track1Selection()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+    //public void track2Selection()
+    //{
+
+    //    SceneManager.LoadScene(3);
+    //}
+
+    public void quitGame()
+    {
+
+        Application.Quit();
+    }
+
+    public void backToMainMenu()
+    {
+
+        SceneManager.LoadScene(0);
+    }
+}
