1722a3fd781a3c3d66bac817a7c024b184dda3ea
Sameer-Ehsanmund <sameer.baigs321@gmail.com>
2021-08-31 12:59:25 -0700

    level designing

---
+++ Mobile Car Racing Game/Assets/Scripts/track03/track3TrackerScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class track3TrackerScript : MonoBehaviour
+{
+    public GameObject theMarker;
+
+    public GameObject[] markers;
+
+    public int markTracker;
+
+    void Update()
+    {
+
+        foreach (GameObject marker in markers)
+        {
+
+            for (int i = 0; i < markers.Length; i++)
+            {
+
+                if (i == markTracker)
+                {
+
+                    theMarker.transform.position = markers[i].transform.position;
+                }
+            }
+        }
+    }
+
+    IEnumerator OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.gameObject.tag == "Dreamcar01")
+        {
+
+            this.GetComponent<BoxCollider>().enabled = false;
+
+            markTracker += 1;
+
+            if (markTracker == 19)
+            {
+
+                markTracker = 0;
+            }
+
+            yield return new WaitForSeconds(1);
+            this.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
